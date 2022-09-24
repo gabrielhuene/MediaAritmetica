0a96afd73d045364da6bdf83a6910d70ab6f8e69
gabriel huene <gabriellaurence05@gmail.com>
2022-09-24 16:05:54 -0300

    adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Média Aritmética de 3 numeros\n");
+
+Console.WriteLine("Digite seu primeiro número ");
+double numero1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite seu segundo némero: ");
+double numero2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite seu terceiro número: ");
+double numero3 = Convert.ToDouble(Console.ReadLine());
+
+double media = (numero1 + numero2 + numero3) / 3;
+
+Console.WriteLine($"\nmédia; {media:N1}");
